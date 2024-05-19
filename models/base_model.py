ef5601f366cbe27d37226dd947c8b1c69733a98c
kerubo19 <philip@DESKTOP-DLBOSKE.localdomain>
2024-05-19 15:14:07 +0300

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,26 @@
 #!/usr/bin/python3
-'''
-    This module defines the BaseModel class
-'''
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
-    '''
-        Base class for other classes to be used for the duration.
-    '''
-    def __init__(self, *args, **kwargs):
-        '''
-            Initialize public instance attributes.
-        '''
-        if (len(kwargs) == 0):
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-        else:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            for key, val in kwargs.items():
-                if "__class__" not in key:
-                    setattr(self, key, val)
 
-    def __str__(self):
-        '''
-            Return string representation of BaseModel class
-        '''
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                      self.id, self.__dict__))
-
-    def __repr__(self):
-        '''
-            Return string representation of BaseModel class
-        '''
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                      self.id, self.__dict__))
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()  # Record the creation time
+        self.updated_at = datetime.now()
 
     def save(self):
-        '''
-            Update the updated_at attribute with new.
-        '''
-        self.updated_at = datetime.now()
-        models.storage.save()
+        self.updated_at = datetime.now()  # Update the 'updated_at' timestamp
+        # Save the object to the data storage (file, database, etc.)
 
     def to_dict(self):
-        '''
-            Return dictionary representation of BaseModel class.
-        '''
-        cp_dct = dict(self.__dict__)
-        cp_dct['__class__'] = self.__class__.__name__
-        cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        # Convert the object to a dictionary
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
 
-        return (cp_dct)
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
